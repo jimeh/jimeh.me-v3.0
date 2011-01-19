bd2e4e1b9f7a63f833cb5bf1bbf16702d522ec94
Jim Myhrberg <contact@jimeh.me>
2011-01-19 22:14:39 +0000

    fixed a bug in Firefox with the webkit scrollbar

--- source/site/javascripts/webkit-scrollbar.js
+++ source/site/javascripts/webkit-scrollbar.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	if (navigator.userAgent.match(/Mobile.*Safari|Safari.*Mobile/i) !== null) {
-		$("body").css("position", "static").css("overflow-y", "auto").css("overflow-x", "auto");
-	};
+  if (navigator.userAgent.match(/Mobile.*Safari|Safari.*Mobile/i) !== null || navigator.userAgent.match(/Safari/) == null) {
+    $("body").css("position", "static").css("overflow-y", "auto").css("overflow-x", "auto").css("right", "");
+  };
 });
