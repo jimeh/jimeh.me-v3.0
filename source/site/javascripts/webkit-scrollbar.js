be019eff0889b37bc072ca8bdf672b0938c3373b
Jim Myhrberg <contact@jimeh.me>
2011-11-01 23:17:37 +0000

    a bit more readable javascript

--- source/site/javascripts/webkit-scrollbar.js
+++ source/site/javascripts/webkit-scrollbar.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  if (navigator.userAgent.match(/Mobile.*Safari|Safari.*Mobile/i) !== null || navigator.userAgent.match(/Safari/) == null) {
-    $("body").css("position", "static").css("overflow-y", "auto").css("overflow-x", "auto").css("right", "");
+  if (navigator.userAgent.match(/Mobile.*Safari|Safari.*Mobile/i) !== null ||
+      navigator.userAgent.match(/Safari/) == null) {
+    $("body")
+          .css("position", "static")
+          .css("overflow-y", "auto")
+          .css("overflow-x", "auto")
+          .css("right", "");
   };
 });
